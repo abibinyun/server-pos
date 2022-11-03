0746e81015e122e14cd02fa48608e923c220d361
bilal <abii.manyun@gmail.com>
2022-11-03 16:47:39 +0700

    update

--- database/index.js
+++ database/index.js
@@ -1,6 +1,13 @@
+// const mongoose = require("mongoose");
+const { dbUrl } = require("../app/config");
+// mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+// const db = mongoose.connection;
+
+// module.exports = db;
+
 const mongoose = require("mongoose");
-const { dbHost, dbPass, dbPort, dbName, dbUser } = require("../app/config");
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+// const { dbHost, dbPass, dbUser } = require("../app/config");
+mongoose.connect(`${dbUrl}`);
 const db = mongoose.connection;
 
 module.exports = db;
